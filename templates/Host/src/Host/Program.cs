8ee90b998666a6d7452c1e3e8ec057c785fefe70
Peter Kneale <PeterKneale@gmail.com>
2024-09-15 09:39:25 +1000

    Add html and css

--- templates/Host/src/Host/Program.cs
+++ templates/Host/src/Host/Program.cs
@@ -1,18 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
 // Example of how to load the web ui from a module
-var assembly = typeof(TenancyAssemblyInfo).Assembly;
-builder.Services.AddRazorPages().AddApplicationPart(assembly).AddRazorRuntimeCompilation();
-builder.Services.Configure<MvcRazorRuntimeCompilationOptions>(options =>
-{
-    options.FileProviders.Add(new EmbeddedFileProvider(assembly));
-});
+// var assembly = typeof(ModularMonolithModuleAssemblyInfo).Assembly;
+// builder.Services.AddRazorPages().AddApplicationPart(assembly).AddRazorRuntimeCompilation();
+// builder.Services.Configure<MvcRazorRuntimeCompilationOptions>(options =>
+// {
+//     options.FileProviders.Add(new EmbeddedFileProvider(assembly));
+// });
   
 var app = builder.Build();
 app.UseStaticFiles();
 app.UseRouting();
 // app.UseAuthentication();
 // app.UseAuthorization();
-app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/meta/name", () => Assembly.GetExecutingAssembly().GetName());
+app.MapGet("/health/alive", () => "alive");
+app.MapGet("/health/ready", () => "ready");
 app.MapRazorPages();
 app.Run();
