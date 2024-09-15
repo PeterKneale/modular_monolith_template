1f45e2fb6f4df64f8c28c85b94a501f51d14ced6
Peter Kneale <PeterKneale@gmail.com>
2024-09-15 22:14:06 +1000

    Fix table name

--- templates/Module/src/ModularMonolithModule/ModularMonolithModule/Application/DbConstants.cs
+++ templates/Module/src/ModularMonolithModule/ModularMonolithModule/Application/DbConstants.cs
@@ -2,9 +2,9 @@
 
 public static class DbConstants
 {
-    public static string Schema = "ModularMonolithModule".ToLowerInvariant();
+    public static string Schema = "ModularMonolithModule".ToLowerInvariant() + "_module";
     public static string IdColumn = "id";
     public static string NameColumn = "name";
     public static string PriceColumn = "price";
-    public static string WidgetsTable = "ModularMonolithModule".ToLowerInvariant();
+    public static string WidgetsTable = "widgets";
 }
